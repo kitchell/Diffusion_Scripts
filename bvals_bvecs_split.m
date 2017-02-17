5d9de60aec85433f454acbde0252e621c0d24504
kitchell <lindseykitchell21@gmail.com>
2017-02-17 11:23:42 -0500

    changes to bvals_bvecs_split

--- bvals_bvecs_split.m
+++ bvals_bvecs_split.m
@@ -7,11 +7,11 @@ function bvals_bvecs_split
 % build paths
 %subj = 'subj_9946 subj_9975 subj_10004 subj_10005 subj_10006 subj_10011 subj_10012 subj_15 subj_10015 subj_10016 subj_10019 subj_10024 subj_10034'; % subject; add all subjects for batch
 %subj = 'subj_9946';
-b_vals = {'1000','2000'}; % separate shells
+b_vals = {'1000','2500'}; % separate shells
 stem = 'data'; % stem used for writing output files
 projdir1 = '/N/dc2/scratch/kitchell/Cadaver_diffusion'; % path to data directory
 
-% Split data into two separate files (BVALS = 1000 and 2000).
+% Split data into two separate files (BVALS = 1000 and 2500).
 
 bvals = dlmread('data.bvals');
 bvals = round(bvals./100)*100;
@@ -20,16 +20,16 @@ dlmwrite('data.bvals',bvals)
 dwi   = niftiRead('data.nii.gz');
 dwi1000 = dwi;
 dwi1000.fname = 'data_b1000.nii.gz';
-dwi2000 = dwi;
-dwi2000.fname = 'data_b2000.nii.gz';
+dwi2500 = dwi;
+dwi2500.fname = 'data_b2500.nii.gz';
 index1000 = (bvals == 1000);
-index2000 = (bvals == 2000);
+index2500 = (bvals == 2500);
 index0    = (bvals == 0);
 
 % Find all indices to each bvalue and B0
 
 all_1000  = or(index1000,index0);
-all_2000  = or(index2000,index0);
+all_2500  = or(index2500,index0);
 dwi1000.data = dwi.data(:,:,:,all_1000);
 dwi1000.dim(4) = size(dwi1000.data,4);
 niftiWrite(dwi1000);
@@ -38,14 +38,14 @@ dlmwrite('data_b1000.bvals',bvals1000);
 bvecs1000 = dlmread('data.bvecs');
 bvecs1000 = bvecs1000(:,all_1000);
 dlmwrite('data_b1000.bvecs',bvecs1000);
-dwi2000.data = dwi.data(:,:,:,all_2000);
-dwi2000.dim(4) = size(dwi2000.data,4);
-niftiWrite(dwi2000);
-bvals2000 = bvals(all_2000);
-dlmwrite('data_b2000.bvals',bvals2000);
-bvecs2000 = dlmread('data.bvecs');
-bvecs2000 = bvecs2000(:,all_2000);
-dlmwrite('data_b2000.bvecs',bvecs2000);
+dwi2500.data = dwi.data(:,:,:,all_2500);
+dwi2500.dim(4) = size(dwi2500.data,4);
+niftiWrite(dwi2500);
+bvals2500 = bvals(all_2500);
+dlmwrite('data_b2500.bvals',bvals2500);
+bvecs2500 = dlmread('data.bvecs');
+bvecs2500 = bvecs2500(:,all_2500);
+dlmwrite('data_b2500.bvecs',bvecs2500);
 
 for ibv = 1:length(b_vals)
 bvecs = fullfile(pwd, sprintf('data_b%s.bvecs',b_vals{ibv}));
